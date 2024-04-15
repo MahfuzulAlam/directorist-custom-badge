e447e87640bb0a775e6edcb1feb1e6f6463cdbe4
MahfuzulAlam <a_mahfuzul@yahoo.com>
2024-04-15 12:30:44 +0600

    init

---
+++ directorist-custom-badge.php
@@ -0,0 +1,28 @@
+<?php
+
+/** 
+ * @package  Directorist - Custom Badge
+ */
+
+/**
+ * Plugin Name:       Directorist - Custom Badge
+ * Plugin URI:        https://wpwax.com
+ * Description:       Best way to creat a custom badge for directorist
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Author:            wpWax
+ * Author URI:        https://wpwax.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       directorist-custom-badge
+ * Domain Path:       /languages
+ */
+
+/* This is an extension for Directorist plugin. It helps using custom code and template overriding of Directorist plugin.*/
+
+/**
+ * If this file is called directly, abrot!!!
+ */
+if (!defined('ABSPATH')) {
+    exit;                      // Exit if accessed
+}
